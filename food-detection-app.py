29f4549ab2ffbfd8687d8e902918620648501475
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-09-18 16:16:51 +0700

    revert back to 4 days old code

--- food-detection-app.py
+++ food-detection-app.py
@@ -2,43 +2,24 @@ import streamlit as st
 import os
 import gdown
 import numpy as np
-import tensorflow as tf
+from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-# --- Config ---
-FILE_ID = "154M5eBFu3mrwicYwlIkK3V8j7aazLzPT"
-MODEL_PATH = "resnet_best_model.tflite"
-IMG_SIZE = (224, 224)
+# Load model
+file_id = "154M5eBFu3mrwicYwlIkK3V8j7aazLzPT"
+path = "resnet_best_model10.keras"
 
-food_classes = ["apel", "apem", "bakpia", "jeruk", "kue pastel", 
-                "lemper", "onde-onde", "putu ayu", "risol", "roti"]
+def load_model():
+    if not os.path.exists("freshness_best_model.keras"):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, path, quiet=False)
+    return load_model(path)
 
-# --- Load TFLite Model ---
-@st.cache_resource
-def load_tflite_model():
-    if not os.path.exists(MODEL_PATH):
-        url = f"https://drive.google.com/uc?id={FILE_ID}"
-        gdown.download(url, MODEL_PATH, quiet=False)
+food_classes = ["apel", "apem", "bakpia", "jeruk", "kue pastel", "lemper", "onde-onde", "putu ayu", "risol", "roti"]
 
-    interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
-    interpreter.allocate_tensors()
-    return interpreter
-
-interpreter = load_tflite_model()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-# --- Preprocessing ---
-def preprocess_image(image: Image.Image):
-    image = image.convert("RGB")
-    image = image.resize(IMG_SIZE)
-    img_array = np.array(image) / 255.0
-    img_array = np.expand_dims(img_array, axis=0).astype(np.float32)
-    return img_array
-
-# --- Streamlit UI ---
 st.title("🍽️ Food Category Detection")
 
+# Upload image
 img_file = st.file_uploader("Upload Foto", type=["jpg", "png", "jpeg"])
 camera_file = st.camera_input("Atau gunakan Kamera")
 
@@ -47,16 +28,11 @@ if img_file or camera_file:
     st.image(img, caption="Gambar Input", use_column_width=True)
 
     # Preprocess
-    x = preprocess_image(img)
+    img_resized = img.resize((224,224))
+    x = np.expand_dims(np.array(img_resized)/255.0, axis=0)
 
     # Predict
-    interpreter.set_tensor(input_details[0]['index'], x)
-    interpreter.invoke()
-    predictions = interpreter.get_tensor(output_details[0]['index'])
-
-    food_pred = np.argmax(predictions, axis=1)[0]
-    confidence = predictions[0][food_pred]
+    food_pred = np.argmax(load_model.predict(x), axis=1)[0]
 
     st.subheader("🔎 Hasil Prediksi")
     st.write("🍔 Food Category:", food_classes[food_pred])
-    st.write(f"📊 Confidence: {confidence*100:.2f}%")
